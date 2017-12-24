64ed82ec4a85a168b9a79263a917e31984dd088d
Sara Vieira <hey@iamsaravieira.com>
2017-12-24 17:51:22 +0000

    router

--- src/NewGame.js
+++ src/NewGame.js
@@ -1,15 +1,13 @@
 import React from 'react'
-import { graphql } from 'react-apollo'
-import gql from 'graphql-tag'
 
-const NewGame = ({ games, loading }) => (
+const NewGame = () => (
   <div className="pa4 flex justify-center">
     <article class="mw5 bg-white br3 pa3 pa4-ns mv3 ba b--black-10 w-25 pa3 mr2">
       <div class="tc">
         <h1 class="f3 mb2 avenir">Team A</h1>
         <h2 class="f5 fw4 gray mt0 avenir">0</h2>
         <button className="f6 link dim br3 ph3 pv2 mb2 dib white bg-blue no-outline">
-					Goal
+            Goal
         </button>
       </div>
     </article>
@@ -18,7 +16,7 @@ const NewGame = ({ games, loading }) => (
         <h1 class="f3 avenir mb2">Team B</h1>
         <h2 class="f5 fw4 avenir gray mt0">0</h2>
         <button className="f6 link dim br3 ph3 pv2 mb2 dib white bg-blue no-outline">
-					Goal
+            Goal
         </button>
       </div>
     </article>
